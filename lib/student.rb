1a76f4e76738691aff23b9c7e4066c820669582d
Shaina C <scailles23@gmail.com>
2021-04-09 04:29:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,6 +20,10 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
+    @twitter= attributes_hash[:twitter]
+    @linkedin= attributes_hash[:linkedin]
+    @github= attributes_hash[:github]
+    @blog= attributes_hash[:blog]
     
   end
 
