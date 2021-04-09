2de689227ea1859767dcf3764b0a1c027fe5b0e4
Shaina C <scailles23@gmail.com>
2021-04-09 04:24:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,9 +14,9 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
-      student = self.new
+      student = self.new(student)
       
-    
+    end
   end
 
   def add_student_attributes(attributes_hash)
